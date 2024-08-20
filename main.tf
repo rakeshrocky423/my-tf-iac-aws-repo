1e8deeca07904ca4371c64b31b0ad7b11e8ccc99
rakeshrocky423 <162771496+rakeshrocky423@users.noreply.github.com>
2024-08-20 09:32:02 +0530

    Update main.tf2

--- main.tf
+++ main.tf
@@ -1,58 +1,87 @@
+# Configure the AWS provider
 provider "aws" {
-  region = var.aws_region
+    region = "ap-south-1"
 }
 
-# Create security group with firewall rules
-resource "aws_security_group" "my_security_group" {
-  name        = var.security_group
-  description = "security group for EC2 instance"
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Outbound from Jenkins server
-  egress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = var.security_group
-  }
+# Create a VPC
+resource "aws_vpc" "terran_vpc" {
+    cidr_block = "10.0.0.0/16"
+    tags = {
+        Name = "terran-vpc"
+    }
 }
 
-# Create AWS EC2 instance
-resource "aws_instance" "myFirstInstance" {
-  ami           = "ami-0ad21ae1d0696ad58"   # AMI IDs must be in quotes
-  key_name      = "terraform"              # Key names must be in quotes
-  instance_type = "t2.micro"               # Instance types must be in quotes
-
-  vpc_security_group_ids = [aws_security_group.my_security_group.id]  # Reference security group by ID
-
-  tags = {
-    Name = "myFirstInstance"  # Tags must be in quotes
-  }
+# Create a Subnet
+resource "aws_subnet" "terran_subnet" {
+    vpc_id                  = aws_vpc.terran_vpc.id
+    cidr_block              = "10.0.1.0/24"
+    availability_zone       = "ap-south-1b"
+    map_public_ip_on_launch = true
+    tags = {
+        Name = "terran-subnet"
+    }
 }
 
-# Create Elastic IP address
-resource "aws_eip" "myFirstInstance" {
-  vpc      = true
-  instance = aws_instance.myFirstInstance.id
+# Create an Internet Gateway
+resource "aws_internet_gateway" "terran_igw" {
+    vpc_id = aws_vpc.terran_vpc.id
+    tags = {
+        Name = "terran-igw"
+    }
+}
 
-  tags = {
-    Name = "my_elastic_ip"
-  }
+# Create a Route Table
+resource "aws_route_table" "terran_rt" {
+    vpc_id = aws_vpc.terran_vpc.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.terran_igw.id
+    }
+    tags = {
+        Name = "terran-rt"
+    }
+}
+
+# Associate Route Table with Subnet
+resource "aws_route_table_association" "terran_rt_association" {
+    subnet_id      = aws_subnet.terran_subnet.id
+    route_table_id = aws_route_table.terran_rt.id
+}
+
+# Create a Security Group
+resource "aws_security_group" "terran_sg" {
+    description = "Allow SSH traffic"
+    vpc_id      = aws_vpc.terran_vpc.id
+
+    ingress {
+        description = "Allow SSH"
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+   
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "terran-sg"
+    }
+}
+
+# Create an EC2 Instance
+resource "aws_instance" "terran_instance" {
+    subnet_id       = aws_subnet.terran_subnet.id
+    ami             = "ami-0ad21ae1d0696ad58"
+    instance_type   = "t2.micro"
+    key_name        = "terraform"
+    security_groups  = [aws_security_group.terran_sg.id]  # Use security_group_ids instead of security_group_id
+
+    tags = {
+        Name = "terraform-instance"
+    }
 }
